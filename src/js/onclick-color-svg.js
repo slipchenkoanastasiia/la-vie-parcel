4eca9b8b8adcae5644e99d7effcc25ef0462d0dc
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2024-05-06 16:39:23 +0300

    add section

---
+++ src/js/onclick-color-svg.js
@@ -0,0 +1,14 @@
+function toggleColor(event) {
+    let svgIcon = event.target; 
+    let isWhite = svgIcon.classList.contains('white-fill');
+
+    if (isWhite) {
+        svgIcon.classList.remove('white-fill');
+        svgIcon.style.fill = 'none'; 
+        svgIcon.style.stroke = 'var(--bg-color)'; 
+    } else {
+        svgIcon.classList.add('white-fill');
+        svgIcon.style.fill = 'white'; 
+        svgIcon.style.stroke = 'none'; 
+    }
+}
